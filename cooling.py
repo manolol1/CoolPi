83425d0d4e6750d1c14b61ad9f51fa0c7e171441
Manolol <manolotv.yt@gmail.com>
2022-01-02 19:55:00 +0100

    Initial commit

---
+++ cooling.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO # type: ignore
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+pwm = GPIO.PWM(14,100)
+
+print("\nPress Ctrl+C to quit \n")
+pwm.start(0)
+
+try:
+    while True:
+        temp = subprocess.getoutput("vcgencmd measure_temp|sed 's/[^0-9.]//g'")
+        if round(float(temp)) >= 70:
+            print("CPU temperature is over 70°C --> Fan speed: 100%")
+            pwm.ChangeDutyCycle(100)
+            time.sleep(20)
+        elif round(float(temp)) >= 60:
+            print("CPU temperature is over 60°C --> Fan speed: 80%")
+            pwm.ChangeDutyCycle(80)
+            time.sleep(15)
+        elif round(float(temp)) >= 50:
+            print("CPU temperature is over 50°C --> Fan speed: 50%")
+            pwm.ChangeDutyCycle(50)
+            time.sleep(15)
+        elif round(float(temp)) >= 45:
+            print("CPU temperature is over 45°C --> Fan speed: 25%")
+            pwm.ChangeDutyCycle(25)
+            time.sleep(15)
+        else:
+            print("CPU temperature is lower than 45°C. Fan speed: 0%")
+            pwm.ChangeDutyCycle(0)
+            time.sleep(15)
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
+    print("Ctrl + C pressed -- Ending program")
