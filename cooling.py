1395e13dc6cfe09b1de622e1bffedeb34459dea7
Manolol <manolol@mailfence.com>
2022-07-21 23:31:56 +0200

    Added Printing current temperature every 20s

--- cooling.py
+++ cooling.py
@@ -12,22 +12,22 @@ pwm.start(0)
 try:
     while True:
         temp = subprocess.getoutput("vcgencmd measure_temp|sed 's/[^0-9.]//g'")
+        
         if round(float(temp)) >= 70:
             print("CPU temperature is over 70°C --> Fan speed: 100%")
             pwm.ChangeDutyCycle(100)
-            time.sleep(30)
         elif round(float(temp)) >= 60:
             print("CPU temperature is over 60°C --> Fan speed: 80%")
             pwm.ChangeDutyCycle(80)
-            time.sleep(20)
         elif round(float(temp)) >= 50:
             print("CPU temperature is over 50°C --> Fan speed: 25%")
             pwm.ChangeDutyCycle(25)
-            time.sleep(20)
         else:
             print("CPU temperature is lower than 50°C. --> Fan speed: 0%")
             pwm.ChangeDutyCycle(0)
-            time.sleep(15)
+        
+        print("Current Temperature: " + temp + "°C")
+        time.sleep(20)
 except KeyboardInterrupt:
     pwm.stop()
     GPIO.cleanup()
