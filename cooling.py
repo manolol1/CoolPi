23f8d0a8f224f3e54dded4c9d1a76a69eb830d50
manolol1 <manolol@mailfence.com>
2024-04-27 17:11:54 +0200

    Add some very basic time tracking

--- cooling.py
+++ cooling.py
@@ -9,10 +9,12 @@ pwm = GPIO.PWM(14,100)
 print("\nPress Ctrl+C to quit \n")
 pwm.start(0)
 
+timeOn = 0
+
 try:
     while True:
         temp = subprocess.getoutput("vcgencmd measure_temp|sed 's/[^0-9.]//g'")
-        
+        timeOn += 20
         if round(float(temp)) >= 70:
             print("CPU temperature is over 70°C --> Fan speed: 100%")
             pwm.ChangeDutyCycle(100)
@@ -25,8 +27,9 @@ try:
         else:
             print("CPU temperature is lower than 60°C. --> Fan speed: 0%")
             pwm.ChangeDutyCycle(0)
-        
+            timeOn -= 20
         print("Current Temperature: " + temp + "°C")
+        print("Fan on since script start: " + timeOn + " seconds")
         time.sleep(20)
 except KeyboardInterrupt:
     pwm.stop()
